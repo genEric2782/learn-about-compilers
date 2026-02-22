b4186e89a07ff2df9064735de190fa71efbaf32e
Eric Conner <econner2782@gmail.com>
2026-02-21 22:37:42 -0500

    another tmp commit as i mull over bad ideas

--- learn-to-compile-c-sharp/ast/parser.cs
+++ learn-to-compile-c-sharp/ast/parser.cs
@@ -8,7 +8,7 @@ public class Parser
     public Parser(List<Token> tokens)
     {
         _tokens = tokens;
-        _position = 0;
+        _position = -1; // Start at 0? 
     }
 
     // is current position is less than the size of the list of tokens get token at value position
@@ -18,7 +18,15 @@ public class Parser
 
     private Token Consume()
     {
-        return _tokens[_position++];
+        // Ignore whitespace token for now if i need it later remoe this and do something else
+        if (_tokens[_position++].Kind == TokenKind.Whitesace.ToString())
+        {
+            return _tokens[_position + 2];
+        }
+        else
+        {
+            return _tokens[_position++];
+        }
     }
 
 
@@ -26,61 +34,92 @@ public class Parser
     public ASTNode ParseExpression()
     {
 
-        var Node1 = createParsedTokenNode();
-
+        if (!Enum.IsDefind(typeof(Operators), peekNextToken.Kind))
+        {
+            var Node1 = createParsedTokenNode();
+        }
+        else
+        {
+            // TODO Do i want to consume here ?
+            var foo = Consume();
+        }
         // Giant if statement? 
 
-        while (currentToken != null)
+        // Current scheme doesnt do order of operations well.... i.e. not at all 
+        while (currentToken.Kind != TokenKind.EOF)
         {
+
             //if (currentToken.NodeType == TokenKind.Integer.ToString()) // gross
-            if (Enum.IsDefind(typeof(Operators), currentToken.Kind))
+            if (Enum.IsDefind(typeof(Operators), peekNextToken.Kind))
             {
                 // for now if this currentoken is an int its safe to assume that for a proper expression the next token needs to be an operation 
+                //if (peekNextToken.Span.Literal == TokenKind.Integer.ToString())
+                //{
+                // TODO maybe needs updating ?
+                var operate = Consume();
+
                 if (peekNextToken.Span.Literal == TokenKind.Integer.ToString())
                 {
-                    var operate = Consume();
-
                     var Node2 = createParsedTokenNode();
+                }
+                else
+                {
+                    // TODO error? 
+                }
 
+                // Make sure there insnt another operand in the string before we make this part of the tree
+                if (Enum.IsDefind(typeof(Operators), peekNextToken.Kind))
+                {
+                    // TODO       
+                    var something = ParseExpression();
+
+
+                    // Old
+                    // var nextOp = Consume();
+
+                    //var Node3 = createParsedTokenNode();
+
+                    //createParsedTreeNode(nextOp, Node2, Node3);
+                }
+                else
+                {
                     createParsedTreeNode(operate, Node1, Node2);
                 }
-                else if ()
-                {
-
-                }// Another operator or paraen
-            }
-            else if ()
-            {
-
-            }
-            else
-            {
-
             }
         }
-    }
-
-    // Creates a Node that will later be added to a tree 
-    public createParsedTokenNode()
-    {
-        var token = Consume();
-
-        return new ASTNode
+            // TODO: maybe make Numbers Enum? 
+            else if (Enum.IsDefind(typeof(), peekNextToken.Kind))
         {
-            NodeType = token.kind,
-            Value = token.Span.Literal
-        };
-    }
 
-    // Creates a node with children 
-    public createParsedTreeNode(Token head, ASTNode LNode, ASTNode RNode)
-    {
-
-        return new ASTNode
+        }
+        else
         {
-            NodeType = head.kind,
-            Value = head.Span.Literal,
-            Children = new List<ASTNode> { LNode, RNode }
-        };
+
+        }
     }
+}
+
+// Creates a Node that will later be added to a tree 
+public createParsedTokenNode()
+{
+    var token = Consume();
+
+    return new ASTNode
+    {
+        NodeType = token.kind,
+        Value = token.Span.Literal
+    };
+}
+
+// Creates a node with children 
+public createParsedTreeNode(Token head, ASTNode LNode, ASTNode RNode)
+{
+
+    return new ASTNode
+    {
+        NodeType = head.kind,
+        Value = head.Span.Literal,
+        Children = new List<ASTNode> { LNode, RNode }
+    };
+}
 }
