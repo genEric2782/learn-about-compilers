dd0d8c0e903ed529eebf14bb7a60e6822e348542
Eric Conner <econner2782@gmail.com>
2026-03-13 22:04:37 -0400

    flipped the IPC

--- learn-to-compile-c-sharp/Program.cs
+++ learn-to-compile-c-sharp/Program.cs
@@ -9,23 +9,8 @@ class Program
     static async Task Main()
     {
 
-        // Start Process to Read lexed output from stdout 
-        // Driver code for testing parser
-        var psi = new ProcessStartInfo
-        {
-            FileName = "cargo",
-            Arguments = "run --quiet",
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true,
-            WorkingDirectory = @"/home/generic/compiler_project/learn-about-compilers/learn-to-compile"
-        };
 
-        var process = Process.Start(psi);
-
-        string output = await process.StandardOutput.ReadToEndAsync();
-
-        process.WaitForExit();
+        string input = await Console.In.ReadToEndAsync();
 
         var options = new JsonSerializerOptions
         {
@@ -34,7 +19,7 @@ class Program
             WriteIndented = true,
         };
 
-        var tokens = JsonSerializer.Deserialize<List<Token>>(output, options);
+        var tokens = JsonSerializer.Deserialize<List<Token>>(input, options);
 
 
         var parser = new Parser(tokens);
--- learn-to-compile/src/main.rs
+++ learn-to-compile/src/main.rs
@@ -1,5 +1,6 @@
 use crate::rslexer::lexer::{TokenKind}; //Token,
 // use serde::{Deserialize, Serialize};
+use std::process::{Command, Stdio};
 use std::io::{self, Write}; // BufRead
 
 mod rslexer;
@@ -10,8 +11,8 @@ mod rslexer;
 // }
 
 fn main() {
-    let input = "7 + 8 + 5"; // Test string 
-    let mut stdout = io::stdout();
+    let input = "7 + 5 + 4 + 3"; // Test string 
+    // let mut stdout = io::stdout();
 
     let mut lexer = rslexer::lexer::Lexer::new(input);
     let mut tokens = Vec::new();
@@ -25,8 +26,20 @@ fn main() {
         idx += 1;
     }
 
+    let mut child = Command::new("dotnet")
+        .arg("run")
+        .current_dir("/home/generic/compiler_project/learn-about-compilers/learn-to-compile-c-sharp")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .spawn()
+        .expect("Failed to start C# process");
+
     // println!("{:?}", serde_json::to_string(&tokens).unwrap());
 
-    serde_json::to_writer_pretty(std::io::stdout(), &tokens).unwrap();
-    let _ = stdout.flush();
+    let stdin = child.stdin.as_mut().expect("Failed to open stdin");
+
+    // serde_json::to_writer_pretty(std::io::stdout(), &tokens).unwrap();
+    serde_json::to_writer_pretty(stdin, &tokens).unwrap();
+    child.wait().unwrap();
+    // let _ = stdout.flush();
 }
